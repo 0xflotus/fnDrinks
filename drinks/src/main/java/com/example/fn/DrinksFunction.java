039ee9ef1006d42411d878c0d243b0c43f08109f
0xflotus <0xflotus@gmail.com>
2018-12-23 23:19:57 +0100

    Update DrinksFunction.java

--- drinks/src/main/java/com/example/fn/DrinksFunction.java
+++ drinks/src/main/java/com/example/fn/DrinksFunction.java
@@ -5,7 +5,7 @@ public class DrinksFunction {
     public DrinkData handleRequest(DrinkData input) {
         DrinkData newDrinkData = new DrinkData();
         newDrinkData.setName((input == null || input.getName() == null || input.getName().isEmpty()) ? "<no name>" : input.getName());
-        newDrinkData.setAlcohol((input == null || input.getAlcohol() == null) ? 0 : input.getAlcohol() + 1);
+        newDrinkData.setAlcohol((input == null || input.getAlcohol() == null) ? 0L : input.getAlcohol() + 1);
         return newDrinkData;
     }
 
